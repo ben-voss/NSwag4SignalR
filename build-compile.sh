7b7ab2b4af474345492a1d15e2d45690f94149f0
Ben Voss <ben.voss@me.com>
2025-12-08 20:47:16 +0000

    Create initial build action

--- build-compile.sh
+++ build-compile.sh
@@ -1,1 +1,4 @@
-docker build --progress=plain --target=compile --file=docker/Dockerfile . 
+VERSION=1.0.0
+GIT_SHA=$(git rev-parse --short HEAD)
+
+docker build --build-arg VERSION="${VERSION}" --build-arg GIT_SHA="${GIT_SHA}" --progress=plain --target=compile --tag=nswag4signalr.compile:latest --file=docker/Dockerfile . 
